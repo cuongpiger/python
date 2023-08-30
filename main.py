3fb846e2dcf48fcfa2743b6f6720bde2a5cee22c
cuongpiger <cuongpigerr@gmail.com>
2023-08-30 11:17:40 +0700

    This is Manh Cuong's commit 🌻.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from webob import Request
 
 app = Flask(__name__)
+
 
 class CustomMiddleware(object):
     def __init__(self, app):
@@ -9,22 +10,27 @@ class CustomMiddleware(object):
 
     def __call__(self, environ, start_response):
         # Create a Request object
-        request = Request(environ)
+        req = Request(environ)
 
         # Modify the headers
-        request.headers['Custom-Header'] = 'Modified Header Value'
+        req.headers['Custom-Header'] = 'Modified Header Value'
 
         # Call the next middleware or application
-        response = request.get_response(self.app)
+        response = req.get_response(self.app)
 
         return response(environ, start_response)
 
+
 app.wsgi_app = CustomMiddleware(app.wsgi_app)
+
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    # Get the modified headers from the request
+    modified_header = request.headers['Custom-Header']
+
+    return f'Hello, World! Modified Header: {modified_header}'
+
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(port=8080, debug=True)
