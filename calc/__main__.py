3f36067745fb42be037feede104af9c852c3da8a
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-03-27 08:50:00 +0700

    This is Manh Cuong's commit 🌻.

---
+++ calc/__main__.py
@@ -0,0 +1,14 @@
+# __main__.py
+
+import click
+
+from . import commands
+
+@click.group()
+def cli():
+    pass
+
+cli.add_command(commands.add)
+cli.add_command(commands.sub)
+cli.add_command(commands.mul)
+cli.add_command(commands.div)
